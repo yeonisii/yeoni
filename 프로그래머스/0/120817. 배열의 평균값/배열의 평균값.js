3a40dbec52240f343e8c1e052d7e76a2359fc848
yeonisii <161574897+yeonisii@users.noreply.github.com>
2024-04-26 11:33:03 +0900

    [level 0] Title: 배열의 평균값, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.js
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.js
@@ -1,6 +1,6 @@
 function solution(numbers) {
-    let sum =0;
-    for (let i = 0; i<numbers.length; i++){
+    let sum=0;
+    for(let i= 0; i<numbers.length;i++){
         sum += numbers[i]
     }
     let answer = sum / numbers.length;
